173e9e0b5a9a2bc17047c66df4f53df1232876da
irin.sg <irin.sg@gutagroup.ru>
2019-10-30 14:43:23 +0300

    Triple trigger

--- createTrigger.sql
+++ createTrigger.sql
@@ -1,15 +1,47 @@
+/* Обновляет продолжительность курса */
 CREATE FUNCTION updateCourseDuration()
     RETURNS TRIGGER
     AS $$
+    DECLARE
+        delta INTERVAL := 0;
+        courseId INTEGER;
     BEGIN
-        UPDATE Courses 
-        SET duration = duration + NEW.duration 
-        WHERE id = NEW.courseId;
-        RETURN NEW;
+        IF NEW IS NOT NULL THEN
+            delta := delta + NEW.duration;
+            courseId := NEW.courseId;
+        END IF;
+
+        IF OLD IS NOT NULL THEN
+            delta := delta - OLD.duration;
+            IF courseId IS NULL THEN
+                courseId := OLD.courseId;
+            END IF;
+        END IF;
+        
+        IF courseId IS NOT NULL THEN
+            UPDATE Courses 
+            SET duration = duration + delta 
+            WHERE id = courseId;
+        END IF;
+
+        RETURN NULL;
     END;
     $$ LANGUAGE plpgsql;
 
-CREATE TRIGGER UpdateCourseDurationOnLessonInsertion
+/* Увеличить продолжительность курса при добавлении занятия */
+CREATE TRIGGER IncreaseCourseDurationOnLessonInsertion
     AFTER INSERT ON Lessons
+    FOR EACH ROW
+    EXECUTE PROCEDURE updateCourseDuration();
+
+/* Изменить продолжительность курса при обновлении занятия */
+CREATE TRIGGER AdjustCourseDurationOnLessonUpdate
+    AFTER UPDATE ON Lessons
+    FOR EACH ROW
+    EXECUTE PROCEDURE updateCourseDuration();
+
+/* Уменьшить продолжительность курса при удалении занятия */
+CREATE TRIGGER DecreaseCourseDurationOnLessonDeletion
+    AFTER DELETE ON Lessons
     FOR EACH ROW
     EXECUTE PROCEDURE updateCourseDuration();
