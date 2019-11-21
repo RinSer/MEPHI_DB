7f535da9732d72bd1b96d53bd6b78dc1e2d80817
irin.sg <irin.sg@gutagroup.ru>
2019-11-21 18:32:14 +0300

    Update seed for queries

--- createProcedure.sql
+++ createProcedure.sql
@@ -49,4 +49,53 @@ BEGIN
         RAISE EXCEPTION 'На курс никто не записался!';
     END IF;
 END;
+/* 
+    Определение стоимости проведения
+    для записи на курс (одного потока)
+*/
+CREATE FUNCTION find_registration_cost(regId INTEGER) 
+RETURNS void AS $$
+DECLARE
+    clientsCount INTEGER;
+    locId INTEGER;
+    totalCost INTEGER;
+BEGIN
+    -- Узнаем число записавшихся
+    SELECT COUNT(*) INTO clientsCount FROM RegistrationClient rc
+    WHERE rc.registrationId = regId;
+    IF clientsCount > 0 THEN
+        -- Находим Студия, зарезервированную под Регистрацию
+        SELECT id, rentCost INTO locId, totalCost
+        FROM Locations l WHERE id = 
+        (SELECT locationId FROM Registrations r WHERE r.id = regId LIMIT 1);
+        IF locId IS NULL THEN 
+            RAISE EXCEPTION 'У регистрации не определена Студия!';
+        ELSE
+            -- Прибавляем стоимость аренды и доставки кухонного оборудования
+            totalCost := totalCost + COALESCE((SELECT SUM(c) FROM
+            (SELECT (les.quantity - COALESCE(loc.quantity, 0))*(e.averageRentalCost + e.deliveryCost) c 
+            FROM LessonEquipment les
+            JOIN Lessons l ON l.id = les.lessonid 
+            JOIN Equipment e ON e.id = les.equipmentid
+            LEFT JOIN (SELECT equipmentid, quantity FROM LocationEquipment 
+            WHERE locationId = locId) loc ON loc.equipmentid = les.equipmentid  
+            WHERE l.courseId = (SELECT courseId FROM Registrations 
+            WHERE id = regId LIMIT 1)) costs), 0)*clientsCount;
+            -- Прибавляем стоимость покупки и доставки продуктов
+            totalCost := totalCost + COALESCE((SELECT SUM(c) FROM
+            (SELECT lf.quantity*(f.averagePrice + f.deliveryCost) c 
+            FROM LessonFood lf 
+            JOIN Lessons l ON l.id = lf.lessonid
+            JOIN Food f ON f.id = lf.foodid 
+            WHERE l.courseId = (SELECT courseId FROM Registrations 
+            WHERE id = regId LIMIT 1)) costs), 0)*clientsCount;
+            -- Обновляем данные регистрации
+            UPDATE Registrations 
+            SET cost = totalCost
+            WHERE id = regId;
+        END IF;
+    ELSE
+        RAISE EXCEPTION 'На курс никто не записался!';
+    END IF;
+END;
 $$ LANGUAGE plpgsql;
