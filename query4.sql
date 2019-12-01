25aee0b0779412777d3216bde2bcb237d55c7ea9
Serj <serj.dukareff@gmail.com>
2019-12-01 05:08:54 +0300

    Materialized view query optimized

--- query4.sql
+++ query4.sql
@@ -1,41 +1,49 @@
 CREATE MATERIALIZED VIEW ClientsStat
 AS
 	-- 4) Для каждого клиента возвращает:
-	WITH cteRegistrations(id, courseid, lastDate, costPerClient)
-	AS (
-		SELECT 
-			regs.id AS id
-			,regs.courseid AS courseid
-			,MAX(lm.lessondate) AS lastDate
-			,ROUND(regs.cost / (SELECT COUNT(*) 
-			FROM RegistrationClient WHERE registrationid = regs.id), 2) AS costPerClient
-		FROM LessonMaster lm JOIN Registrations regs ON regs.id = lm.registrationid
-		-- уже проведенные
-		WHERE regs.schedule < NOW()
-		GROUP BY regs.id
-	)
-	SELECT 
-		-- идентификатор клиента,
-		c.id
-		-- количество посещенных курсов,
-		,COUNT(r.*) AS CoursesCount
-		-- количество посещенных занятий,
-		,(SELECT COUNT(*) FROM RegistrationClient rc
-		JOIN cteRegistrations r ON r.id = rc.registrationid
-		RIGHT JOIN Lessons l ON l.courseid = r.courseid
-		WHERE clientid = c.id) AS LessonsCount
-		-- количество разных мастеров,
-		,(SELECT COUNT(DISTINCT masterid) FROM registrationmaster
-		WHERE registrationid = ANY(array_agg(r.id))) AS MastersCount
+	SELECT c.*
 		-- количество непереносимостей,
-		,(SELECT COUNT(*) FROM Intolerances
-		WHERE clientid = c.id) AS IntolerancesCount
-		-- потраченные деньги,
-		,SUM(r.costPerClient) AS MoneySpent
-		-- дата последнего посещения курсов
-		,to_char(MAX(r.lastDate), 'DD.MM.YYYY') AS LastLessonDate
-	FROM Clients c
-	RIGHT JOIN RegistrationClient rc ON rc.clientid = c.id
-	JOIN cteRegistrations r ON rc.registrationid = r.id
-	GROUP BY rc.clientid, c.id
-	ORDER BY c.id
+		,COUNT(i.id)
+	FROM (
+		WITH cteClientRegistrations(
+			clientid, 
+			courseid, 
+			lastDate, 
+			costPerClient, 
+			lessonsCount,
+			mastersCount
+		)
+		AS (
+			SELECT 
+				rc.clientid AS clientid
+				,regs.courseid AS courseid
+				,MAX(lm.lessondate) AS lastDate
+				,ROUND(regs.cost / COUNT(rc.clientid) OVER (PARTITION BY regs.id), 2) AS costPerClient
+				,COUNT(DISTINCT lm.lessonid)
+				,COUNT(DISTINCT lm.masterid)
+			FROM LessonMaster lm 
+			JOIN Registrations regs ON regs.id = lm.registrationid
+			JOIN RegistrationClient rc ON rc.registrationid = regs.id
+			-- уже проведенные
+			WHERE regs.schedule < NOW()
+			GROUP BY regs.id, rc.clientid
+		)
+		SELECT 
+			-- идентификатор клиента,
+			cr.clientid
+			-- количество посещенных курсов,
+			,COUNT(cr.courseid) AS CoursesCount
+			-- количество посещенных занятий,
+			,SUM(cr.lessonsCount) AS LessonsCount
+			-- количество разных мастеров,
+			,SUM(cr.mastersCount) AS MastersCount
+			-- потраченные деньги,
+			,SUM(cr.costPerClient) AS MoneySpent
+			-- дата последнего посещения курсов
+			,to_char(MAX(cr.lastDate), 'DD.MM.YYYY') AS LastLessonDate
+		FROM cteClientRegistrations cr
+		GROUP BY cr.clientid
+		ORDER BY cr.clientid
+	) c
+	JOIN Intolerances i ON i.clientid = c.clientid
+	GROUP BY 1, 2, 3, 4, 5, 6;
